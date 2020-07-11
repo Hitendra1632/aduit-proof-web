3f1b74185f5a86633026dc34dc633b0def44c73a
ketz1989 <ketan.141289@gmail.com>
2020-07-11 20:05:09 +0530

    Sign up API binding Paid Plan template

---
+++ src/app/paid-plans/paid-plans.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paid-plans',
+  templateUrl: './paid-plans.component.html',
+  styleUrls: ['./paid-plans.component.scss']
+})
+export class PaidPlansComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
