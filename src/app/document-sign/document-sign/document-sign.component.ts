b99cb78efc05cd08490b75e9ca465679030c6004
ketz1989 <ketan.141289@gmail.com>
2020-07-16 01:55:04 +0530

    Document Sign Template Done

--- src/app/document-sign/document-sign/document-sign.component.ts
+++ src/app/document-sign/document-sign/document-sign.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-document-sign',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-sign.component.scss']
 })
 export class DocumentSignComponent implements OnInit {
-
-  constructor() { }
+  public step = 1;
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
+
+  public previousStep(stepNumber) {
+    if (stepNumber > 1) {
+      this.step--;
+    } else {
+      this.step = 1;
+    }
+  }
+
+  public nextStep(stepNumber) {
+    if (stepNumber >= 1 && stepNumber < 5) {
+      this.step++;
+    } else {
+      this.step = 1;
+    }
+  }
+
+
+  public submitSignatureDoc() {
+    this.step = 5;
+  }
+
+
+  // Navigate to DAshoard
+  public navigateToDashboard() {
+    this.router.navigate(['/dashboard/']);
+  }
+
 
 }
