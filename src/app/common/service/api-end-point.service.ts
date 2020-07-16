47a231e476a4141e0f4b16992921f60328a56585
ketz1989 <ketan.141289@gmail.com>
2020-07-16 23:16:57 +0530

    Application API end-points created

--- src/app/common/service/api-end-point.service.ts
+++ src/app/common/service/api-end-point.service.ts
@@ -7,35 +7,108 @@ export class ApiEndPointService {
 
   constructor() { }
 
-  
+  /*************************** Authentication - Authorization  ************************* */
+
   // User Login
-  userLogin(){
-    return environment.apiBaseURL + '/v1/auth/local/login'
+  userLogin() {
+    return environment.apiBaseURL + '/v1/auth/local/login';
   }
 
-   // User Logout
-   userLogout(){
-    return environment.apiBaseURL + '/user/logout'
+  // User Logout
+  userLogout() {
+    return environment.apiBaseURL + '/user/logout';
   }
 
   // User Registration
-  registerUser(){
-    return environment.apiBaseURL + '/v1/signup'
+  registerUser() {
+    return environment.apiBaseURL + '/v1/signup';
   }
 
+  /***************************  Authentication - Authorization  ************************* */
+
+
+  /***************************  User Information  **************************/
+
   // Fetch User Details
-  getUserDetails(){
-    return environment.apiBaseURL + '/v1/user'
+  getUserDetails() {
+    return environment.apiBaseURL + '/v1/user';
   }
 
   // Update User Record
-  updateUserData(){
-    return environment.apiBaseURL + '/user'
+  updateUserData() {
+    return environment.apiBaseURL + '/user';
   }
 
   // Get User Balance
-  getUserBalance(){
-    return environment.apiBaseURL + '/v1/userBalance'
+  getUserBalance() {
+    return environment.apiBaseURL + '/v1/userBalance';
   }
+
+  /***************************  User Information  **************************/
+
+
+  /***************************  Documents   ***************************/
+
+  // Get Document List
+  getDocumentList() {
+    return environment.apiBaseURL + '/v1/documents';
+  }
+
+  // POST Initialise Doc Sign 
+  postInitialDocSigning() {
+    return environment.apiBaseURL + '/v1/document/signInit';
+  }
+
+  // Get Partial Doc Sign 
+  getPartialDocSign() {
+    return environment.apiBaseURL + '/v1/document/validation';
+  }
+
+  // POST Full Doc Sign 
+  postFullDocSigning() {
+    return environment.apiBaseURL + '/v1/document/validation';
+  }
+
+  // POST Final Doc Sign 
+  postFinalDocSignature() {
+    return environment.apiBaseURL + '/v1/document/signFinalize';
+  }
+
+  /**************************  Documents   ************************* */
+
+
+  /*************************** Plans n Payments   **************************/
+
+  // Get User Payment history
+  getUserPaymentDetails() {
+    return environment.apiBaseURL + '/v1/payments';
+  }
+
+  // Get Plans
+  getPlansList() {
+    return environment.apiBaseURL + '/v1/plans';
+  }
+
+  // Get PAyment Gateway
+  getPaymentGateWayList() {
+    return environment.apiBaseURL + '/v1/paymentGateway';
+  }
+
+  // Gateway Callback link
+  postGateWayCallback() {
+    return environment.apiBaseURL + '/v1/pay0k/callback';
+  }
+
+  // Gateway Status
+  postGateWayStatus() {
+    return environment.apiBaseURL + '/v1/pay0k/statusUpdate';
+  }
+
+  // Buy Plan 
+  postBuyPlan() {
+    return environment.apiBaseURL + '/v1/plan/buy';
+  }
+
+  /***************************  Plans n Payments   **************************/
 
 }
