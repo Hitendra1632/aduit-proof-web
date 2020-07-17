29f942402c101a4d132f2510fb7e7ae103282dc3
ketz1989 <ketan.141289@gmail.com>
2020-07-17 17:09:35 +0530

    Plan cards binded with API

--- src/app/common/service/payment.service.ts
+++ src/app/common/service/payment.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { ApiEndPointService } from './api-end-point.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
-  constructor() { }
+  constructor(
+    private apiEndPointService: ApiEndPointService,
+    private http: HttpClient,
+  ) { }
+
+  // Get Plans List
+  public getPlanList() {
+    const httpHeaders = new HttpHeaders();
+    const httpOptions = {
+      headers: httpHeaders,
+      withCredentials: true,
+      observe: 'response' as 'response',
+    };
+    return this.http.get(this.apiEndPointService.getPlansList(), httpOptions)
+      .pipe(map((plan) => {
+        return plan.body;
+      }));
+  }
+
+  // Get Payment Gateway List
+  public getGatewayList() {
+    const httpHeaders = new HttpHeaders();
+    const httpOptions = {
+      headers: httpHeaders,
+      withCredentials: true,
+      observe: 'response' as 'response',
+    };
+    return this.http.get(this.apiEndPointService.getPaymentGateWayList(), httpOptions)
+      .pipe(map((payment) => {
+        return payment.body;
+      }));
+  }
+
+  
+  // Get User Payment History 
+  public getUserPaymentHistoryList() {
+    const httpHeaders = new HttpHeaders();
+    const httpOptions = {
+      headers: httpHeaders,
+      withCredentials: true,
+      observe: 'response' as 'response',
+    };
+    return this.http.get(this.apiEndPointService.getUserPaymentDetails(), httpOptions)
+      .pipe(map((payHistory) => {
+        return payHistory.body;
+      }));
+  }
 }
--- src/app/paid-plans/paid-plans.component.ts
+++ src/app/paid-plans/paid-plans.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from '../common/service/user.service';
+import { PaymentService } from '../common/service/payment.service';
 
 @Component({
   selector: 'app-paid-plans',
@@ -9,19 +10,76 @@ import { UserService } from '../common/service/user.service';
 })
 export class PaidPlansComponent implements OnInit {
 
+  public planSet = {};
+  public isPlanSetLoading = false;
+  public paymentGatewaySet = {};
+  public userPaymentData = {};
+  public userSelectedPlan = {};
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private userService: UserService,
+    private paymentService: PaymentService
   ) { }
 
   ngOnInit(): void {
-    //this.getUserDetails();
+    this.getUserHistoryDetails();
+    this.getAvailablePlans();
+    this.getAvailablePaymentGateways();
   }
 
-  public getUserDetails(){
-    this.userService.getUserDetails().subscribe(userResult => {
-          console.log(userResult);
-        })
+  //Fetch the plans avaiable
+  public getAvailablePlans() {
+    this.isPlanSetLoading = true;
+    this.paymentService.getPlanList().subscribe(plans => {
+      this.planSet = plans;
+      this.isPlanSetLoading = false;
+    }, error => {
+      this.isPlanSetLoading = false;
+    })
+  }
+
+  //Fetch the avaiable payment Gateways
+  public getAvailablePaymentGateways() {
+    this.paymentService.getGatewayList().subscribe(payGateway => {
+      // this.paymentGatewaySet = payGateway;
+    }, error => {
+
+    });
+  }
+
+  // User Payment History
+  public getUserHistoryDetails() {
+    this.paymentService.getUserPaymentHistoryList().subscribe(userHistory => {
+      console.log(userHistory);
+      // this.userPaymentData = userHistory;
+    }, error => {
+
+    })
+  }
+
+  // Selected Plan 
+  public selectedPlan(e, planItem) {
+    this.userSelectedPlan = planItem;
+  }
+
+  // Highlight user selected Plan 
+  highlightSelectedPlan(pItem, area) {
+    if (area === 'border') {
+      // 'Card'border style
+      if (this.userSelectedPlan) {
+        return this.userSelectedPlan['planID'] === pItem.planID ? 'border-green-500' : 'border-blue-500';
+      } else {
+        return 'border-blue-500';
+      }
+    } else {
+      // 'Choose' button style
+      if (this.userSelectedPlan) {
+        return this.userSelectedPlan['planID'] === pItem.planID ? 'bg-green-500' : 'bg-blue-500';
+      } else {
+        return 'bg-blue-500';
+      }
+    }
   }
 }
